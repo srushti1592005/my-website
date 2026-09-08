cb8df718599378b13afcfdce398bd33b524c36c8
srushti kadam <sachin@tbstdigital.com.au>
2026-09-08 21:56:25 +0530

    Added image slider

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Website Loaded");
+const images=[
+"https://images.unsplash.com/photo-1497366754035-f200968a6e72?w=1600",
+"https://images.unsplash.com/photo-1497366216548-37526070297c?w=1600",
+"https://images.unsplash.com/photo-1520607162513-77705c0f0d4a?w=1600"
+];
+
+let i=0;
+
+setInterval(()=>{
+i=(i+1)%images.length;
+document.getElementById("slide").src=images[i];
+},3000);
